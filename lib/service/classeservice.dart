ffd6a59cf01dbf1f3b37c4f6d637fcea088d8a91
compte <stagiaire@neopolis-dev.com>
2023-11-14 23:02:30 +0100

    gestion universite v.3 flutter

---
+++ lib/service/classeservice.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http ;
+import 'package:http/http.dart';
+
+import 'package:clienttp07/entities/Student.dart';
+import 'package:clienttp07/entities/classe.dart';
+import 'package:clienttp07/entities/student.dart';
+
+Future getAllClasses() async{
+
+  Response response =await  http.get(Uri.parse("http://10.0.2.2:8091/class/all"));
+  return jsonDecode(response.body);
+
+}
+
+Future deleteClass(int id){
+  return http.delete(Uri.parse("http://10.0.2.2:8091/class/delete?id=${id}"));
+}
+
+
+Future addClass(Classe classe)async
+{
+
+  Response response = await http.post(Uri.parse("http://10.0.2.2:8081/class/add"),
+      headers:{
+        "Content-type":"Application/json"
+      },body:jsonEncode(<String,dynamic>{
+        "nomClass":classe.nomClass,
+        "nbreEtud":classe.nbreEtud
+      }
+      ));
+
+  return response.body;
+}
+
+Future updateClasse(Classe classe)async
+{
+
+  Response response = await http.put(Uri.parse("http://10.0.2.2:8081/class/update"),
+      headers:{
+        "Content-type":"Application/json"
+      },body:jsonEncode(<String,dynamic>{
+        "codClass":classe.codClass,
+        "nomClass":classe.nomClass,
+        "nbreEtud":classe.nbreEtud
+      }
+      ));
+
+  return response.body;
+}
+
+Future getAllStudents() async {
+  Response response = await http.get(Uri.parse("http://10.0.2.2:8091/etudiant/all"));
+  return jsonDecode(response.body);
+}
